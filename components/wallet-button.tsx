f8e5e17f51d5f3769d0b8fb2f65441b0c83843de
Tim Borovkov <tim.borovkov@icloud.com>
2025-03-14 16:52:36 -0700

    Memo wallet button

--- components/wallet-button.tsx
+++ components/wallet-button.tsx
@@ -1,7 +1,9 @@
+import { memo } from 'react';
+
 import { WalletDefault } from '@coinbase/onchainkit/wallet';
 
 const WalletButton = () => {
   return <WalletDefault />;
 };
 
-export default WalletButton;
+export default memo(WalletButton);
